3c6b0e3793bfca86b635dbbafbc42d3bee59aa77
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2023-01-19 15:25:47 +0100

    K2/Native: support cinterop forward declarations
    Support importing synthetic declarations from "magic" forward
    declaration packages:
    - cnames.structs
    - objcnames.classes
    - objcnames.protocols
    
    So this is a rough equivalent to the forward declarations module
    made of ForwardDeclarationsPackageFragmentDescriptor in K1.
    
    Unlike K1 implementation, this K2 one doesn't allow importing
    a declaration that wasn't actually forward-declared in a C/Objective-C
    header available through a cinterop klib dependency.

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirNativeSessionFactory.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirNativeSessionFactory.kt
@@ -39,6 +39,7 @@ object FirNativeSessionFactory : FirAbstractSessionFactory() {
             createProviders = { session, builtinsModuleData, kotlinScopeProvider ->
                 listOf(
                     KlibBasedSymbolProvider(session, moduleDataProvider, kotlinScopeProvider, resolvedLibraries),
+                    NativeForwardDeclarationsSymbolProvider(session, moduleDataProvider, kotlinScopeProvider, resolvedLibraries),
                     FirBuiltinSymbolProvider(session, builtinsModuleData, kotlinScopeProvider),
                     FirCloneableSymbolProvider(session, builtinsModuleData, kotlinScopeProvider),
                 )
