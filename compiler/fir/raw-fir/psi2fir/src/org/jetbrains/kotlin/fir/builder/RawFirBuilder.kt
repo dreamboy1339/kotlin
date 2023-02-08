aebb33733619746a34d77e25442f9b7e4cea2a67
Egor Kulikov <egor.kulikov@jetbrains.com>
2023-02-08 02:39:19 +0100

    [FIR] Use AST nodes only for lambda parameter names
    Eighth step for ^KT-52615

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -578,11 +578,7 @@ open class RawFirBuilder(
             valueParameterDeclaration: ValueParameterDeclaration,
             additionalAnnotations: List<FirAnnotation>,
         ): FirValueParameter {
-            val name = if (valueParameterDeclaration == ValueParameterDeclaration.LAMBDA) {
-                convertValueParameterName(nameAsSafeName, nameIdentifier?.node?.text, valueParameterDeclaration)
-            } else {
-                nameAsSafeName
-            }
+            val name = convertValueParameterName(nameAsSafeName, valueParameterDeclaration) { nameIdentifier?.node?.text }
             return buildValueParameter {
                 source = toFirSourceElement()
                 moduleData = baseModuleData
@@ -2108,9 +2104,8 @@ open class RawFirBuilder(
                     val parameter = clause.catchParameter?.let { ktParameter ->
                         val name = convertValueParameterName(
                             ktParameter.nameAsSafeName,
-                            ktParameter.nameIdentifier?.node?.text,
                             ValueParameterDeclaration.CATCH
-                        )
+                        ) { ktParameter.nameIdentifier?.node?.text }
 
                         buildProperty {
                             source = ktParameter.toFirSourceElement()
